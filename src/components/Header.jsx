09ae7b6214ced740ed86d835c4bb67b91513c66f
Владислав А. Сосков <VASoskov@center.omgtu>
2023-09-08 11:07:45 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return(
+  
+      <header className='header'> 
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/2491px-Logo_of_Twitter.svg.png"></img>
+        </header>
+  )
+}
+  
+  export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://w.forfun.com/fetch/5d/5d94ad27b68ed87ab6bda49e3b90f923.jpeg"></img>
+      </div>
+      <div>ava</div>
+      <div>My post</div>
+      <div>New post</div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+        <div>post 3</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
