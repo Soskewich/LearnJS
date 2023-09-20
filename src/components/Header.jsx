feb10c4c8a1c794bce7bcf9dff1ce76219be0db2
Soskov Vladislav <soskovvlad@gmail.com>
2023-09-20 22:52:06 +0600

    14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import classes from "./Header.module.css"
 const Header = () => {
   return(
   
-      <header className='header'> 
+      <header className={classes.header}> 
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/2491px-Logo_of_Twitter.svg.png"></img>
         </header>
   )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-
+import classes from "./Profile.module.css"
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={classes.content}>
       <div>
         <img src="https://w.forfun.com/fetch/5d/5d94ad27b68ed87ab6bda49e3b90f923.jpeg"></img>
       </div>
       <div>ava</div>
       <div>My post</div>
       <div>New post</div>
-      <div>
-        <div>post 1</div>
-        <div>post 2</div>
-        <div>post 3</div>
+      <div className="post">
+        <div className="item">post 1</div>
+        <div className="item">post 2</div>
+        <div className="item">post 3</div>
       </div>
     </div>
   );
