efbd1d13d8b9a64a4f7c89a16f11d04d5d01370f
Владислав А. Сосков <VASoskov@center.omgtu>
2023-09-22 16:57:24 +0600

    15

---
+++ src/components/Profile/MyPost/Post/Post.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Post.module.css"
+const Post = () => {
+  return (
+
+        <div className={classes.item}>
+            <img src="https://cdni.comss.net/logo/Telegram.png" alt=""/>
+            post 1
+        </div>
+
+  );
+};
+
+export default Post;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./Profile.module.css"
+import MyPosts from "./MyPost/MyPosts";
 const Profile = () => {
   return (
     <div className={classes.content}>
@@ -7,13 +8,7 @@ const Profile = () => {
         <img src="https://w.forfun.com/fetch/5d/5d94ad27b68ed87ab6bda49e3b90f923.jpeg"></img>
       </div>
       <div>ava</div>
-      <div>My post</div>
-      <div>New post</div>
-      <div className="post">
-        <div className="item">post 1</div>
-        <div className="item">post 2</div>
-        <div className="item">post 3</div>
-      </div>
+        <MyPosts />
     </div>
   );
 };
