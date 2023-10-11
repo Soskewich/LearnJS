53508c9026ef68672b15963ed9b212143f2bbdba
Владислав А. Сосков <VASoskov@center.omgtu>
2023-10-11 15:15:49 +0600

    37

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { renderEntireTree } from './render';
-import state from './redux/state';
+import {BrowserRouter} from "react-router-dom";
 
-renderEntireTree(state);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import store from "./redux/state";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let renderEntireTree = (state) => {
+    root.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={store.getState()} addPost={store.addPost.bind(store)} updatePostText={store.updatePostText.bind(store)} />
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals();
+
+}
+renderEntireTree(store._state);
+store.subscribe(renderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,51 +1,70 @@
-import { renderEntireTree } from "../render";
+import {getMessage} from "@testing-library/jest-dom/dist/utils";
 
-let state = {
-    profilesPage:{
-        postsData: [
-    
-            {id: 1, message: 'its my first post', likescount: 12},
-            {id: 2, message: 'its my second post', likescount: 18},
-            {id: 3, message: 'its my thirty post', likescount: 24},
-          
-          ],
-        
-        newPostText:"write text" 
+let store = {
+    _state: {
+        profilesPage:{
+            postsData: [
 
-        
-                
+                {id: 1, message: 'its my first post', likescount: 12},
+                {id: 2, message: 'its my second post', likescount: 18},
+                {id: 3, message: 'its my thirty post', likescount: 24},
+
+            ],
+
+            newPostText:"write text"
+
+
+
+        },
+        messagesPage: {
+            messageData: [
+                {id: 1, message:"hi"},
+                {id: 2, message:"HI"},
+                {id: 3, message:"Hello"},
+                {id: 4, message:"Hello!"},
+            ],
+            dialogsData: [
+                {id: 1, name:"Tema"},
+                {id: 2, name:"Valera"},
+                {id: 3, name:"Serega"},
+                {id: 4, name:"Danik"},
+            ],
+        }
+
     },
-    messagesPage: {
-        messageData: [
-            {id: 1, message:"hi"},
-            {id: 2, message:"HI"},
-            {id: 3, message:"Hello"},
-            {id: 4, message:"Hello!"},
-          ],
-          dialogsData: [
-            {id: 1, name:"Tema"},
-            {id: 2, name:"Valera"},
-            {id: 3, name:"Serega"},
-            {id: 4, name:"Danik"},
-          ],  
+    getState(){
+        return this._state
+    },
+    _callSubscriber(){
+        console.log('change done')
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilesPage.newPostText,
+            likescount: 100
+        }
+        this._state.profilesPage.postsData.push(newPost);
+        this._state.profilesPage.newPostText = ''
+        this._callSubscriber(this._state);
+    },
+    updatePostText(newText){
+        this._state.profilesPage.newPostText = newText
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
-      
-  }
-
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilesPage.newPostText,
-        likescount: 100
-    }
-    state.profilesPage.postsData.push(newPost);
-    state.profilesPage.newPostText = ''
-    renderEntireTree(state);
 }
 
-export let updatePostText = (newText) => {
-    state.profilesPage.newPostText = newText
-    renderEntireTree(state);
-}
 
-  export default state;
+
+
+
+
+
+
+
+
+  export default store;
+
